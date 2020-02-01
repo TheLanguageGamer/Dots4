977460ba2063549f5652992f1c068d04a373c959
TheLanguageGamer <justin.m.helms+1@gmail.com>
2020-02-01 05:40:17 -0800

    different icons

--- src/ts/ImagePaths.ts
+++ src/ts/ImagePaths.ts
@@ -1,10 +1,10 @@
 
 const ImagePaths = {
 	Icons : <Record<string, string>> {
-		"Play" : "images/icons/icons8-play-100.png",
-		"Pause" : "images/icons/icons8-pause-100.png",
+		"Play" : "images/icons/icons8-play-52.png",
+		"Pause" : "images/icons/icons8-pause-90.png",
 		"Checkmark" : "images/icons/icons8-checkmark-104.png",
-		"Download" : "images/icons/icons8-download-100.png",
+		"Download" : "images/icons/icons8-download-52.png",
 		"Upload" : "images/icons/icons8-upload-52.png",
 	},
 };
