a402cf03fefeaf8b2634d349bc9894053d9b3343
TheLanguageGamer <justin.m.helms+1@gmail.com>
2020-02-04 18:14:09 -0800

    Container component .. no opt render, can hold other components

---
+++ src/js/Components/Container.js
@@ -0,0 +1,9 @@
+"use strict";
+class Container {
+    constructor() {
+        this.layout = new Layout(0, 0, 0, 0, 1, 1, 0, 0);
+    }
+    render(ctx, cp) {
+    }
+}
+//# sourceMappingURL=Container.js.map
---
+++ src/ts/Components/Container.ts
@@ -0,0 +1,13 @@
+class Container {
+	
+	layout : Layout;
+	children? : Component[];
+
+	constructor() {
+		this.layout = new Layout(0, 0, 0, 0, 1, 1, 0, 0);
+	}
+
+	render(ctx : CanvasRenderingContext2D, cp : ContentProvider) {
+		
+	}
+}
