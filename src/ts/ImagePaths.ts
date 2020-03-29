1167c329922514194d89add2894104067ef1ee5f
TheLanguageGamer <justin.m.helms+1@gmail.com>
2020-03-29 07:55:03 -0700

    corrected image paths

--- src/ts/ImagePaths.ts
+++ src/ts/ImagePaths.ts
@@ -8,9 +8,9 @@ const ImagePaths = {
 		"Upload" : "images/icons/icons8-upload-52.png",
 		"Trash" : "images/icons/icons8-trash-208.png",
 		"Plus" : "images/icons/icons8-plus-math-52.png",
-		"Left" : "images/state/icons8-left-96.png",
-		"Right" : "images/state/icons8-right-96.png",
-		"Up" : "images/state/icons8-up-96.png",
-		"Down" : "images/state/icons8-down-arrow-96.png",
+		"Left" : "images/icons/icons8-left-96.png",
+		"Right" : "images/icons/icons8-right-96.png",
+		"Up" : "images/icons/icons8-up-96.png",
+		"Down" : "images/icons/icons8-down-arrow-96.png",
 	},
 };
